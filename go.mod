5c2169430ec439af2db39c43e4d85308fd5b7e52
Calvin Behling <calvin.behling@gmail.com>
2022-12-20 19:10:40 -0600

    try making this a module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TripleDogDare/license-test
+
+go 1.18
---
+++ hello.go
@@ -0,0 +1,8 @@
+package licensetest
+
+import ("fmt")
+
+// SayHello prints a hello world statement.
+func SayHello() {
+	fmt.Println("Hello, World!")
+}
